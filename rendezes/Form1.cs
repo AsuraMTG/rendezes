e82a11a5f083a7e91ec9e3aa426ad224f068b5cf
Masznyik Marton <masmar575@logiker.hu>
2024-10-16 11:04:28 +0200

    Add project files.

---
+++ rendezes/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rendezes
+{
+    public partial class Form1 : Form
+    {
+        int[] szamok = new int[20];
+        int counter = 0;
+        int seged = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                szamok[i] = rnd.Next(1, 101);
+            }
+
+            for (int k = 0; k < szamok.Length; k++)
+            {
+                for (int i = 0; i < szamok.Length; i++)
+                {
+                    counter = 0;
+                    for (int j = 0; j < szamok.Length; j++)
+                    {
+                        if (szamok[i] > szamok[j])
+                        {
+                            counter++;
+                        }
+                    }
+                    seged = szamok[i];
+                    szamok[i] = szamok[counter];
+                    szamok[counter] = seged;
+                }
+            }
+
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                label1.Text += $"{szamok[i]} ";
+            }
+
+        }
+    }
+}
